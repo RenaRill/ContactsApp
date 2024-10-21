fefea8504f466790f757db8882f976a69c05b682
Alena Kirillova <66324245+RenaRill@users.noreply.github.com>
2024-10-21 21:46:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,5 @@ def delete_contact(id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Создаем таблицы
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
+
