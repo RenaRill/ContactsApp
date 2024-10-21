8d626a7455223e4c05607f3933a1387f189cdee2
Alena Kirillova <66324245+RenaRill@users.noreply.github.com>
2024-10-21 21:14:15 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contacts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(15), nullable=False)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/contacts', methods=['GET'])
+def get_contacts():
+    contacts = Contact.query.all()
+    return render_template('contacts.html', contacts=contacts)
+
+@app.route('/contacts', methods=['POST'])
+def add_contact():
+    data = request.json
+    new_contact = Contact(name=data['name'], phone=data['phone'])
+    db.session.add(new_contact)
+    db.session.commit()
+    return jsonify({'id': new_contact.id}), 201
+
+@app.route('/contacts/<int:id>', methods=['PUT'])
+def update_contact(id):
+    data = request.json
+    contact = Contact.query.get_or_404(id)
+    contact.name = data['name']
+    contact.phone = data['phone']
+    db.session.commit()
+    return jsonify({'id': contact.id})
+
+@app.route('/contacts/<int:id>', methods=['DELETE'])
+def delete_contact(id):
+    contact = Contact.query.get_or_404(id)
+    db.session.delete(contact)
+    db.session.commit()
+    return jsonify({'result': True})
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Создаем таблицы
+    app.run(debug=True)
